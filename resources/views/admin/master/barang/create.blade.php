a8bf51ee67edecb640722d239cc5b3ae73604a69
Winar Joko Alexander <winaralex417@gmail.com>
2024-03-20 14:13:42 +0700

    update label nama

--- resources/views/admin/master/barang/create.blade.php
+++ resources/views/admin/master/barang/create.blade.php
@@ -49,7 +49,7 @@
                         <div class="fv-row mb-7 fv-plugins-icon-container">
                             <!--begin::Label-->
                             <div class="col-md-4">
-                                <x-forms.input id="nama" label="Nama Barang" :required="'required'" :name="'nama'" :type="'text'" :placeholder="'Masukkan nama barang...'" :value="''" />
+                                <x-forms.input id="nama" label="Nama" :required="'required'" :name="'nama'" :type="'text'" :placeholder="'Masukkan nama barang...'" :value="''" />
                                 <x-forms.input-error name="nama" />
                             </div>
                         </div>
