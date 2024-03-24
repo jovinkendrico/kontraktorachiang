f3ab4fb73374a6e6913d05cad2676ccb8a05bf4d
jovinkendrico <jovinkendrico@gmail.com>
2024-03-24 11:00:14 +0700

    add belongs to and hasmany kaskeluar.php

--- app/Models/KasKeluar.php
+++ app/Models/KasKeluar.php
@@ -4,11 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class KasKeluar extends Model
 {
+    use HasFactory, SoftDeletes;
+
     protected $table = 'kas_keluars';
     protected $fillable = ['tanggal', 'project_id', 'deskripsi'];
-    use HasFactory, SoftDeletes;
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function detailcashkeluar(): HasMany
+    {
+        return $this->hasMany(DetailCashKeluar::class);
+    }
 }
