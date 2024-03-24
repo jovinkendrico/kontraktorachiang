a5958c1c8b6191dc15476d4dca0da5e7cf1e9092
jovinkendrico <jovinkendrico@gmail.com>
2024-03-24 10:57:06 +0700

    add belongsto to detailpembelianbarang.php

--- app/Models/DetailPembelianBarang.php
+++ app/Models/DetailPembelianBarang.php
@@ -4,14 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DetailPembelianBarang extends Model
 {
+    use HasFactory, SoftDeletes;
 
     protected $table = 'detail_pembelian_barangs';
 
     protected $fillable = ['pembelian_barang_id', 'barang_id', 'merk', 'qty', 'uom', 'harga'];
 
-    use HasFactory, SoftDeletes;
+    public function pembelianbarang(): BelongsTo
+    {
+        return $this->belongsTo(PembelianBarang::class);
+    }
+
+    public function barang(): BelongsTo
+    {
+        return $this->belongsTo(PembelianBarang::class);
+    }
 }
