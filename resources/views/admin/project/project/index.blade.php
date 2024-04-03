d63d1c4e34ce4b4968f72830d5df015ddf0f3f64
jovinkendrico <jovinkendrico@gmail.com>
2024-04-03 15:05:08 +0700

    add get total payment at model and view

--- resources/views/admin/project/project/index.blade.php
+++ resources/views/admin/project/project/index.blade.php
@@ -72,6 +72,7 @@
                                 <th class="min-w-125px">Total Harga</th>
                                 <th class="min-w-200px">Total Pembelian Barang</th>
                                 <th class="min-w-125px">Total Jasa</th>
+                                <th class="min-w-125px">Total Payment</th>
                                 <th class="min-w-125px">Profit</th>
                                 <th class="min-w-125px">Status</th>
                                 <th class="min-w-125px">Actions</th>
@@ -101,6 +102,9 @@
                                     </td>
                                     <td class="text-gray-800 text-hover-primary mb-1">
                                         Rp. {{ number_format($project->getTotalJasa(), 0, ',', '.') }}
+                                    </td>
+                                    <td class="text-gray-800 text-hover-primary mb-1">
+                                        Rp. {{ number_format($project->getTotalPayment(), 0, ',', '.') }}
                                     </td>
                                     <td class="text-gray-800 text-hover-primary mb-1">
                                         Rp. {{ number_format($project->getProfit(), 0, ',', '.') }}
